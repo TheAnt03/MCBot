e8f173da61df3144550ec0cf1454fad6ec744eb0
antmar03 <99343801+antmar03@users.noreply.github.com>
2024-05-12 03:48:26 -0300

    Passes server into bot object

--- src/main/java/uraniumape/mcbot/Bot.java
+++ src/main/java/uraniumape/mcbot/Bot.java
@@ -2,6 +2,7 @@ package uraniumape.mcbot;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Server;
 import org.openjdk.nashorn.api.scripting.NashornScriptEngineFactory;
 import uraniumape.mcbot.script.responses.Message;
 
@@ -12,12 +13,14 @@ public class Bot  {
     private String script = "";
     private String name;
     private String botPrefix;
+    private Server server;
 
 
     public Bot(String name, String script) {
         this.name = name;
         this.botPrefix = "[" + ChatColor.translateAlternateColorCodes('&', name) + "§f]";
         this.script = script;
+        this.server = MCBot.getInstance().getServer();
 
         engine = new NashornScriptEngineFactory().getScriptEngine();
     }
@@ -37,6 +40,10 @@ public class Bot  {
 
     public String getName() {
         return name;
+    }
+
+    public Server getServer() {
+        return server;
     }
 
 }

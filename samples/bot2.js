ad866a5b1c77bc1b4e7fa2fb12f66321bed0a08e
antmar03 <99343801+antmar03@users.noreply.github.com>
2024-05-12 05:18:30 -0300

    Updates sample 2

--- samples/bot2.js
+++ samples/bot2.js
@@ -1,31 +1,50 @@
 function getPlayersMessage(players) {
-	var message = "The current players on the server ar2e:\n";
-	
+	var message = "The current players on the server are:\n";
+
 	for(var i = 0; i < players.length; i++) {
 		var playerName = players[i].getName();
 
 		message += playerName + "\n";
+		message += playerName + "\n";
+		message += playerName + "\n";
 	}
-	
+
 	return message;
 }
 
 function onMessageSend(bot, message) {
 	var content = message.getContent();
 	var player = message.getPlayer();
-	
+
 	switch(content) {
-		case "!hello":
+		case "!test2":
 			bot.sendMessage("Hello " + player.getName());
-			
+
 			break;
 		case "!getActivePlayers":
 			var server = bot.getServer();
 			var players = server.getOnlinePlayers();
 			var message = getPlayersMessage(players);
-
 			bot.sendMessage(message);
 
 			break;
+		case "!getRandomWord":
+			var response = bot.get("https://random-word-api.herokuapp.com/word");
+			var responseJSON = JSON.parse(response);
+
+			bot.sendMessage(responseJSON);
+			break;
 	}
+}
+
+function onPlayerJoin(bot, player) {
+	var message = "";
+
+	if(player.hasPlayedBefore()) {
+		message = "Welcome back " + player.getName();
+	} else {
+		message = "Welcome " + player.getName();
+	}
+
+	bot.sendMessage(message);
 }

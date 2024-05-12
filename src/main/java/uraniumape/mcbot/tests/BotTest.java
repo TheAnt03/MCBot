0b79f70404d70bd730506aff1aff118d4035de00
antmar03 <99343801+antmar03@users.noreply.github.com>
2024-05-12 02:32:17 -0300

    Fixes bot eval test

--- src/main/java/uraniumape/mcbot/tests/BotTest.java
+++ src/main/java/uraniumape/mcbot/tests/BotTest.java
@@ -2,6 +2,7 @@ package uraniumape.mcbot.tests;
 
 import org.junit.jupiter.api.Test;
 import uraniumape.mcbot.Bot;
+import uraniumape.mcbot.script.responses.Message;
 import uraniumape.mcbot.storage.Bots;
 import java.util.List;
 
@@ -10,13 +11,19 @@ import javax.script.ScriptException;
 
 public class BotTest {
 
-    //Does not work.
     @Test
     public void testEval() throws ScriptException, NoSuchMethodException {
-        List<Bot> bots = Bots.getInstance().getBots();
+        Message message = new Message(null, "!test");
+        String testScript =
+                "function onMessageSend(bot, message) {" +
+                    "var content = message.getContent();" +
+                    "if(content.equals(\"!test\")) {" +
+                        "print(\"eval is working\");" +
+                    "}" +
+                "}";
 
-        for(Bot bot : bots) {
-            bot.readMessage("test");
-        }
+        Bot bot = new Bot("Test Bot", testScript);
+
+        bot.readMessage(message);
     }
 }
